4848e4c24fde76ece1afb5780224b4eb97f638cf
Courtney Phillips <quirkyfiddie@yahoo.com>
2021-01-13 22:06:03 -0600

    -m "Chapter 3 exercises + Studio + Bonus"

--- src/org/launchcode/java/studios/countingcharacters/Counting.java
+++ src/org/launchcode/java/studios/countingcharacters/Counting.java
@@ -1,29 +1,42 @@
 package org.launchcode.java.studios.countingcharacters;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class Counting {
     public static void main(String[] args) {
+        Path filePath = Paths.get("C:\\Users\\Courtney\\Desktop","lorem.txt");
+        try {
+            String phraseInit = Files.readString(filePath);
+            String phrase = phraseInit.replaceAll("[^a-zA-Z]", "").toLowerCase();
+            char[] charactersInString = phrase.toCharArray();
 
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter text to check: ");
-        String phraseInit = input.nextLine().toLowerCase();
-        String phrase = phraseInit.replaceAll("[^a-zA-Z]", "");
-        char[] charactersInString = phrase.toCharArray();
-
-        Map<Character, Integer> occurrences = new HashMap<>();
-        for (char letter : charactersInString) {
-            if (occurrences.containsKey(letter)) {
-                occurrences.put(letter, occurrences.get(letter)+1);
-            }
-            else {
-                occurrences.put(letter, 1);
+            Map<Character, Integer> occurrences = new HashMap<>();
+            for (char letter : charactersInString) {
+                if (occurrences.containsKey(letter)) {
+                    occurrences.put(letter, occurrences.get(letter)+1);
+                }
+                else {
+                    occurrences.put(letter, 1);
+                }
+            } for (Map.Entry entry : occurrences.entrySet()) {
+                System.out.println(entry.getKey() + ":" + entry.getValue());
             }
         }
-    for (Map.Entry entry : occurrences.entrySet()) {
-        System.out.println(entry.getKey() + ":" + entry.getValue());
-    }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        /*Scanner input = new Scanner(System.in);
+        System.out.println("Enter text to check: ");
+        String phraseInit = input.nextLine().toLowerCase();
+        input.close();*/
+
+
     }
 }
