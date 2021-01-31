6b7891d1232cf54e555b9e3f7b1f9c41ce4808fd
Courtney Phillips <quirkyfiddie@yahoo.com>
2021-01-31 15:30:53 -0600

    -m "Chapter 5 exercises"

--- src/org/launchcode/java/demos/lsn3classes1/Student.java
+++ src/org/launchcode/java/demos/lsn3classes1/Student.java
@@ -9,5 +9,50 @@ public class Student {
     private int studentId;
     private int numberOfCredits = 0;
     private double gpa = 0.0;
+    public Student(String name, int studentId,
+                   int numberOfCredits, double gpa) {
+        this.name = name;
+        this.studentId = studentId;
+        this.numberOfCredits = numberOfCredits;
+        this.gpa = gpa;
+    }
+
+    public Student(String name, int studentId) {
+        this.name = name;
+        this.studentId = studentId;
+        this.numberOfCredits = 0;
+        this.gpa = 0.0;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String aName) {
+        name = aName;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int aStudentId) {
+        studentId = aStudentId;
+    }
+
+    private int getNumberOfCredits() {
+        return numberOfCredits;
+    }
+
+    private void setNumberOfCredits(int aNumberOfCredits) {
+        numberOfCredits = aNumberOfCredits;
+    }
+
+    private double getGpa() {
+        return gpa;
+    }
+
+    private void setGpa(double aGpa) {
+        gpa = aGpa;
+    }
 
 }
---
+++ src/org/launchcode/java/demos/lsn3classes1/Teacher.java
@@ -0,0 +1,5 @@
+package org.launchcode.java.demos.lsn3classes1;
+
+public class Teacher {
+
+}
